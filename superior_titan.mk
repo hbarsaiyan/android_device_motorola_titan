683915cef585fb4c89ca117e08af442737ee1019
hbarsaiyan <37732050+hbarsaiyan@users.noreply.github.com>
2020-02-20 11:29:35 +0000

    Update superior_titan.mk

--- superior_titan.mk
+++ superior_titan.mk
@@ -18,4 +18,6 @@ $(call inherit-product, vendor/superior/config/common.mk)
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := superior_titan
 
+TARGET_BOOT_ANIMATION_RES := 720
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
